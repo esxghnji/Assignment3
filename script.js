12b233138a043ebd9c94c05200024ca760608c6a
DMN-DUDE <144633958+DMN-DUDE@users.noreply.github.com>
2023-09-15 14:14:51 +1000

    added more stuff

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
+const slides = document.querySelectorAll(".slide");
 
+slides.forEach((slide, indx) => {
+  slide.style.transform = `translateX(${indx * 100}%)`;
+});
+
+let curSlide = 0;
+
+const nextSlide = document.querySelector(".btn-next");
+
+nextSlide.addEventListener("click", function () {
+     curSlide++;
+
+  slides.forEach((slide, indx) => {
+    slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
+  });
+});
+
+const nextSlide = document.querySelector(".btn-next");
+
+let curSlide = 0;
+let maxSlide = slides.length - 1;
+
+nextSlide.addEventListener("click", function () {
+  if (curSlide === maxSlide) {
+    curSlide = 0;
+  } else {
+    curSlide++;
+  }
+
+  slides.forEach((slide, indx) => {
+    slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
+  });
+});
